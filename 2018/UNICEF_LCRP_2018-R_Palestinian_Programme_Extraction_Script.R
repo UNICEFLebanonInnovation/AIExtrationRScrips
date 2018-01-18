02bf4144f95beedc91cac1f98001e8c3cdcde79b
achamseddine <achamseddine@unicef.org>
2018-01-18 15:08:17 +0200

    Adding the new 2018 script for PLP

---
+++ 2018/UNICEF_LCRP_2018-R_Palestinian_Programme_Extraction_Script.R
@@ -0,0 +1,76 @@
+
+library("ggplot2")
+opt <- options(warn = -1)
+rm(list = ls())
+
+library("activityinfo")
+
+# Replace 'NA' with the numeric identifier of your database (e.g. 1234):
+database.id <- 6352
+
+# Uncomment the following command if you want to log in manually, leave commented
+# out if you have stored your login credentials on your local machine.
+
+if (is.na(database.id)) {
+  stop("you forgot to set the database identifier at the top of this script!")
+}
+values <- getDatabaseValueTable(database.id)
+
+# Generating values dataframe 
+cat("Done. The results are in a data frame called 'values'.\n")
+
+# Subsetting Values to UNICEF interventions dataset only 
+values$start_date <-strftime(values$start_date,"%Y-%m")
+
+subset_data1 = list(
+  values[["indicator.id"]],
+  values[["indicator.name"]],
+  values[["indicator.value"]],
+  values[["indicator.units"]],
+  values[["start_date"]],
+  values[["partner.label"]],
+  values[["partner.description"]],
+  values[["location.adminlevel.governorate"]],
+  values[["location.adminlevel.caza"]],
+  values[["location.adminlevel.cadastral_area"]],
+  values[["location.adminlevel.cadastral_area.code"]],
+  values[["indicator.category"]],
+  values[["location.latitude"]],
+  values[["location.longitude"]],
+  values[["location.alternate_name"]],
+  values[["location.adminlevel.caza.code"]],
+  values[["Site Type"]]
+)
+
+names(subset_data1) <- c("Indicator_ID", "Indicator_Name", "Value", "Unit", "Month", "Partner_Name", "PartnerFullName", 
+                         "Governorate", "Caza", "Cadaster", "CadCod","Indicator_Category", "Location_Latitude", 
+                         "Location_Longitude", "locationName", "location.adminlevel.caza.code", "Site_Type")
+
+subset_data2 = list(
+  values[["indicator.id"]],
+  values[["indicator.name"]],
+  values[["location.adminlevel.governorate"]]
+)
+
+names(subset_data2) <- c("Indicator_ID", "Indicator_Dissegregation", 
+                         "Location")
+
+# Creating a folder and export the extraction as a csv file to "DBs" folder after creating it in case it doesn't exist
+db.all.lcrp <- values
+
+outfilname<- paste(Sys.Date(), "_Sector_PLP.csv", sep="")
+write.csv(db.all.lcrp,outfilname)
+
+# Creating a folder and export the extraction as a csv file to "UNICEF" folder after creating the folder in case it doesn't exist
+db.12columns.lcrp <- subset_data1
+
+outfilname<- paste(Sys.Date(), "_UNICEF_PBI_PPL.csv", sep="")
+write.csv(db.12columns.lcrp,outfilname)
+cat("Done. The results of UNICEF interventions are in a data frame called 'UNICEF'.\n")
+
+db.6columns.lcrp <- subset_data2
+
+outfilname<- paste(Sys.Date(), "_UNICEF_PLP.csv", sep="")
+write.csv(db.6columns.lcrp,outfilname)
+cat("Done. The results of UNICEF interventions are in a data frame called 'UNICEF'.\n")
+
