09425405c54aa1a4e755aba961414ea1d8971aa9
achamseddine <achamseddine@unicef.org>
2018-01-18 15:31:12 +0200

    Remove row numbering

--- 2018/UNICEF_LCRP_2018-R_Palestinian_Programme_Extraction_Script.R
+++ 2018/UNICEF_LCRP_2018-R_Palestinian_Programme_Extraction_Script.R
@@ -59,18 +59,18 @@ names(subset_data2) <- c("Indicator_ID", "Indicator_Dissegregation",
 db.all.lcrp <- values
 
 outfilname<- paste(Sys.Date(), "_Sector_PLP.csv", sep="")
-write.csv(db.all.lcrp,outfilname)
+write.csv(db.all.lcrp, outfilname, row.names=FALSE)
 
 # Creating a folder and export the extraction as a csv file to "UNICEF" folder after creating the folder in case it doesn't exist
 db.12columns.lcrp <- subset_data1
 
 outfilname<- paste(Sys.Date(), "_UNICEF_PBI_PPL.csv", sep="")
-write.csv(db.12columns.lcrp,outfilname)
+write.csv(db.12columns.lcrp, outfilname, row.names=FALSE)
 cat("Done. The results of UNICEF interventions are in a data frame called 'UNICEF'.\n")
 
 db.6columns.lcrp <- subset_data2
 
 outfilname<- paste(Sys.Date(), "_UNICEF_PLP.csv", sep="")
-write.csv(db.6columns.lcrp,outfilname)
+write.csv(db.6columns.lcrp, outfilname, row.names=FALSE)
 cat("Done. The results of UNICEF interventions are in a data frame called 'UNICEF'.\n")
 
